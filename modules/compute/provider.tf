cd28696919073f9c23a8523d03142d22137341fd
Dorin <your.email@example.com>
2026-01-07 12:44:02 +0200

    facem debug

--- modules/compute/provider.tf
+++ modules/compute/provider.tf
@@ -11,5 +11,6 @@ provider "oci" {
   user_ocid = "ocid1.user.oc1..aaaaaaaasfs5r4eij324barnbwkwcghkg53qg7w4s2but3rvtthfjozgh2zq"
   fingerprint = var.fingerprint
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaouminldguaddw4th2rysvt2wezzx2nscn6snhkrc72dn6wjs27gq"
-  private_key_path = var.private_key_path
+  # private_key_path = var.private_key_path
+  private_key      = var.private_key
 }
--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -16,8 +16,14 @@ type        = string
 sensitive   = true
 }
 
-variable "private_key_path" {
+/* variable "private_key_path" {
   type        = string
   description = "Path to your private API key file"
+  sensitive   = true
+} */ 
+
+variable "private_key" {
+  description = "Content of the private API key (PEM format)"
+  type        = string
   sensitive   = true
 }
