bf8afd47041e386c1fff1836e72afcf943b6018f
Dorin <your.email@example.com>
2026-01-07 18:40:31 +0200

    alabala

--- backend.tf
+++ backend.tf
@@ -3,8 +3,6 @@ terraform {
     bucket   = "terraform-state-bucket"
     key      = "terraform.tfstate"
     region   = "us-ashburn-1"
-    user_ocid = "ocid1.user.oc1..aaaaaaaasfs5r4eij324barnbwkwcghkg53qg7w4s2but3rvtthfjozgh2zq"
-    namespace = "ocicustomeropshb"
-    tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaouminldguaddw4th2rysvt2wezzx2nscn6snhkrc72dn6wjs27gq"
+    namespace = ocicustomeropshb
   }
 }
