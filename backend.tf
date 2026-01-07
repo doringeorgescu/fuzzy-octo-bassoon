ed26e0b38182ddf477300d39eaa86cbcbdf9cf5d
Dorin <your.email@example.com>
2026-01-07 18:39:02 +0200

    alabala

--- backend.tf
+++ backend.tf
@@ -4,9 +4,7 @@ terraform {
     key      = "terraform.tfstate"
     region   = "us-ashburn-1"
     user_ocid = "ocid1.user.oc1..aaaaaaaasfs5r4eij324barnbwkwcghkg53qg7w4s2but3rvtthfjozgh2zq"
-    fingerprint = var.fingerprint
-    private_key      = var.private_key 
-    namespace = ocicustomeropshb
+    namespace = "ocicustomeropshb"
     tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaouminldguaddw4th2rysvt2wezzx2nscn6snhkrc72dn6wjs27gq"
   }
 }
