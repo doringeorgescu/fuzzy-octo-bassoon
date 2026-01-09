ee6b663a278c07923d72d099a82b1fe33805ac08
Dorin <your.email@example.com>
2026-01-09 19:15:32 +0200

    Gata, facem instanta finala

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vm1" {
   source       = "./modules/compute"
-  nume_instanta = "Instanta Plutoshica"
+  nume_instanta = "Instanta Pluto"
   subnetID = oci_core_subnet.subnet_pluto.id
   fingerprint = var.fingerprint
   # private_key_path = var.private_key_path
