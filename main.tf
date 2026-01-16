fa9902a57cf7b1278e19bb03df5247ab46c31192
Dorin <your.email@example.com>
2026-01-16 16:30:20 +0200

    am facut instanta cu Linux

--- main.tf
+++ main.tf
@@ -1,8 +1,1 @@
-module "vm1" {
-  source       = "./modules/compute"
-  nume_instanta = "Instanta cu Linux"
-  subnetID = oci_core_subnet.subnet_pluto.id
-  fingerprint = var.fingerprint
-  # private_key_path = var.private_key_path
-  private_key = var.private_key
-}
+
